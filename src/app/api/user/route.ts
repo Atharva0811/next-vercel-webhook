cd28596bd20c800a40fa6c80d8fb6c4c32c3330d
Atharva0811 <atharvaus008@gmail.com>
2025-02-12 16:27:17 +0530

    body storing post message

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -4,8 +4,10 @@ import user from "../../model/user";
 export async function POST(request: Request) {
   try {
     await dbConnect();
+    const body = await request.json();
+    
     console.log("Webhook received!");
-    await user.create({ name: "John Doe" });
+    await user.create({ name: JSON.stringify(body)});
     // Process the webhook payload
   } catch (error) {
     return new Response(`Webhook error: ${error}`, {
